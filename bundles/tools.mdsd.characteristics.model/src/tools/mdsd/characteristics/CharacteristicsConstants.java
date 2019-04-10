5fa9589ebf1f1ee82b11dfa7e9cb74dce9e923f8
Sebastian Krach <krach@fzi.de>
2019-04-10 11:33:45 +0200

    Autoload base types in characteristic editor #4

--- bundles/tools.mdsd.characteristics.model/src/tools/mdsd/characteristics/CharacteristicsConstants.java
+++ bundles/tools.mdsd.characteristics.model/src/tools/mdsd/characteristics/CharacteristicsConstants.java
@@ -4,9 +4,11 @@ import org.eclipse.emf.common.util.URI;
 
 public final class CharacteristicsConstants {
 	public static final String URI_SCHEME = "characteristics";
-	public static final String URI_DEFAULT_IDENTIFIER = "/module/tools.mdsd.characteristics.provider.baseproperties.BasePropertiesExtensionModule";
+	public static final String URI_DEFAULT_IDENTIFIER = "/module/tools.mdsd.characteristics.api.baseproperties.BasePropertiesExtensionModule";
 	
-	public static final URI REALM_INSTANCE_OR_DEFAULT = URI.createURI(String.join(":", URI_SCHEME, URI_DEFAULT_IDENTIFIER)); 
+	public static final URI REALM_INSTANCE_OR_DEFAULT = URI.createURI(String.join(":", URI_SCHEME, URI_DEFAULT_IDENTIFIER));
+	
+	public static final URI BASE_VALUE_TYPE_MODEL = URI.createURI("pathmap://CHARACTERISTICS_RESOURCES/basetypes.valuetype");
 
 	private CharacteristicsConstants() {}
 	
