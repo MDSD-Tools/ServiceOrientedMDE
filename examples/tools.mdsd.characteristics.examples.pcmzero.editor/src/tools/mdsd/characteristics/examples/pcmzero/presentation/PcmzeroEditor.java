0987dba4a376efd8d93a1b34afd5ca72f996a93b
Sebastian Krach <krach@fzi.de>
2019-04-10 11:32:53 +0200

    Extended PCMZero example with string characteristic #4

---
+++ examples/tools.mdsd.characteristics.examples.pcmzero.editor/src/tools/mdsd/characteristics/examples/pcmzero/presentation/PcmzeroEditor.java
@@ -0,0 +1,38 @@
+/**
+ */
+package tools.mdsd.characteristics.examples.pcmzero.presentation;
+
+import org.eclipse.emf.edit.provider.ComposedAdapterFactory;
+
+import tools.mdsd.characteristics.CharacteristicsConstants;
+import tools.mdsd.characteristics.edit.support.CharacteristicsSupportExtension;
+import tools.mdsd.characteristics.edit.support.ExtensibleDispatchingItemProviderDecoratorFactory;
+
+/**
+ * This is an example of a Pcmzero model editor.
+ */
+public class PcmzeroEditor
+	extends PcmzeroEditorGen
+{
+	
+	@Override
+	protected void initializeEditingDomain() {
+		super.initializeEditingDomain();
+		
+		ExtensibleDispatchingItemProviderDecoratorFactory exFact = 
+				new ExtensibleDispatchingItemProviderDecoratorFactory(adapterFactory);
+		CharacteristicsSupportExtension.registerCharacteristicSupportExtensions(exFact);
+		
+		adapterFactory = new ComposedAdapterFactory(exFact);
+		editingDomain.setAdapterFactory(adapterFactory);
+	}
+	
+	@Override
+	public void createModel() {
+		super.createModel();
+		
+		//FIXME: This is a hack until we have properly resolved API initialization
+		editingDomain.getResourceSet().getResource(CharacteristicsConstants.REALM_INSTANCE_OR_DEFAULT,
+				true);
+	}
+}
