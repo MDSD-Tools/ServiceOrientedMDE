003e6058197688711689866bf8764d553b439a4d
Sebastian Krach <krach@fzi.de>
2019-05-05 18:04:38 +0200

    Simplified API lookup

--- bundles/tools.mdsd.characteristics.model/src/tools/mdsd/characteristics/CharacteristicsConstants.java
+++ bundles/tools.mdsd.characteristics.model/src/tools/mdsd/characteristics/CharacteristicsConstants.java
@@ -3,15 +3,11 @@ package tools.mdsd.characteristics;
 import org.eclipse.emf.common.util.URI;
 
 public final class CharacteristicsConstants {
-	public static final String URI_SCHEME = "characteristics";
-	public static final String URI_DEFAULT_IDENTIFIER = "/module/tools.mdsd.characteristics.api.baseproperties.BasePropertiesExtensionModule";
-	
-	public static final URI REALM_INSTANCE_OR_DEFAULT = URI.createURI(String.join(":", URI_SCHEME, URI_DEFAULT_IDENTIFIER));
 	
 	public static final URI BASE_VALUE_TYPE_MODEL = URI.createURI("pathmap://CHARACTERISTICS_RESOURCES/basetypes.valuetype");
-
+	
+	public static final String MODELING_REALM_FILE_EXTENSION = "modelingrealm";
+	
 	private CharacteristicsConstants() {}
 	
-	
-
 }
--- bundles/tools.mdsd.characteristics.model/src/tools/mdsd/characteristics/utils/ValueTypeUtils.java
+++ bundles/tools.mdsd.characteristics.model/src/tools/mdsd/characteristics/utils/ValueTypeUtils.java
@@ -1,12 +1,7 @@
 package tools.mdsd.characteristics.utils;
 
-import java.util.Optional;
-
 import org.eclipse.emf.ecore.EDataType;
-import org.eclipse.emf.ecore.resource.Resource;
 
-import tools.mdsd.characteristics.CharacteristicsConstants;
-import tools.mdsd.characteristics.api.CharacteristicsModelingRealm;
 import tools.mdsd.characteristics.valuetype.ValueType;
 import tools.mdsd.characteristics.valuetype.impl.ValueTypeImpl;;
 
@@ -15,11 +10,9 @@ public final class ValueTypeUtils {
 	private ValueTypeUtils() {}
 	
 	public static <P> P getProperty(ValueType vt, Class<P> propertyType) {
-		return Optional.ofNullable(vt.eResource().getResourceSet().getResource(CharacteristicsConstants.REALM_INSTANCE_OR_DEFAULT, true))
-				.map(Resource::getContents).filter(c -> !c.isEmpty()).map(c -> c.get(0))
-				.map(CharacteristicsModelingRealm.class::cast)
-				.map(CharacteristicsModelingRealm::getValueTypePropertiesService)
-				.flatMap(vtps -> vtps.getProperty(propertyType, vt))
+		return CharacteristicsAPIAware.lookupCharacteristicsModelingRealm(vt)
+				.getValueTypePropertiesService()
+				.getProperty(propertyType, vt)
 				.orElse(null);
 	}
 	
@@ -32,14 +25,9 @@ public final class ValueTypeUtils {
 	}
 
 	public static boolean hasProperty(ValueType vt, Class<?> propertyType) {
-		
-		return Optional.ofNullable(vt.eResource().getResourceSet().getResource(CharacteristicsConstants.REALM_INSTANCE_OR_DEFAULT, true))
-			.map(Resource::getContents).filter(c -> !c.isEmpty()).map(c -> c.get(0))
-			.map(CharacteristicsModelingRealm.class::cast)
-			.map(CharacteristicsModelingRealm::getValueTypePropertiesService)
-			.flatMap(vtps -> vtps.getProperty(propertyType, vt))
-			.isPresent();
-		
+		return CharacteristicsAPIAware.lookupCharacteristicsModelingRealm(vt)
+				.getValueTypePropertiesService()
+				.getProperty(propertyType, vt).isPresent();
 	}
 
 }
